d820276f7bfc109a18656bb31c32441ecc732cc8
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-06-14 21:29:12 +0500

    update signature/function overload

--- index.ts
+++ index.ts
@@ -140,7 +140,7 @@ console.log("Get Function: ", p1.name)
 p1.name = "Musab Ahmed"
 console.log("Set Function: ", p1.name)
 
-// Signature Or Function overloaded --> but its function name are same so it will work.
+// Signature Or Function overloaded --> but its function name are same so it will work to maintain its type. :any -> where TypeScripts not work.
 
 function abc(a:number, b:number):void
 function abc(a:string, b:string):void
